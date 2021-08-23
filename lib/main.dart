43d7c0067383f719045afe51f64a4d02f83b9855
anup-paul <anupdesktopit@gmail.com>
2021-08-23 14:01:22 +0600

    add textbutton

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ void main() {
     home: Scaffold(
       backgroundColor: Colors.yellow,
       appBar: AppBar(
+        centerTitle: true,
         title: Text(
           'Dice',
           style: TextStyle(
@@ -23,6 +24,33 @@ class DicePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextButton(
+                onPressed: () {
+                  print('Left Button cliked');
+                },
+                child: Image.asset('images/dice1.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextButton(
+                onPressed: () {
+                  print('Right Button is pressed');
+                },
+                child: Image.asset('images/dice2.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
